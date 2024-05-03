5f0d374bed931930609eacfb85a3b4108eb863a8
Cristian Camilo Martínez Soto <ccmartinez@SAVIASALUDEPS.LOCAL>
2024-05-03 14:02:12 -0500

    Comentario Componente principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Home } from "./master/home/pages/Home";
-import { WhatsAppIcon } from "./master/icons/WhatsAppIcon";
 
 export const App = () => {
   return (
     <>
+    {/* Componente inicial */}
       <Home />
     </>
   );
